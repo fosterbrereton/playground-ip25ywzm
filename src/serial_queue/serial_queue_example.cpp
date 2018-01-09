6dd4a27cce95b90f95c8579130cd0f4658d8212b
Foster Brereton <fbrereto@adobe.com>
2018-01-08 17:26:34 -0800

    works locally

--- src/serial_queue/serial_queue_example.cpp
+++ src/serial_queue/serial_queue_example.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include <stlab/concurrency/serial_queue.hpp>
+#include <stlab/concurrency/default_executor.hpp>
 
 using namespace std;
 using namespace stlab;
 
 void serial_queue_example() {
-    cout << "Hello, world!\n";
+    serial_queue_t(default_executor).executor()([]{
+        cout << "Hello, world!\n";
+    });
 }
